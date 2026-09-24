5b62f359b8b5976c55ceef2fe28f38cb0f3b5c4c
PatrycjaL <landaspatrycja@gmail.com>
2026-09-24 06:15:50 +0200

    Rozbudowanie API usług i dodanie walidacji

---
+++ src/main/java/pl/landas/systemrezerwacji/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package pl.landas.systemrezerwacji.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> obsluzBladWalidacji(MethodArgumentNotValidException exception) {
+
+        Map<String, String> bledy = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(blad -> {
+            bledy.put(blad.getField(), blad.getDefaultMessage());
+        });
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(bledy);
+    }
+}
--- src/main/java/pl/landas/systemrezerwacji/repository/UslugaRepository.java
+++ src/main/java/pl/landas/systemrezerwacji/repository/UslugaRepository.java
@@ -3,5 +3,8 @@ package pl.landas.systemrezerwacji.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.landas.systemrezerwacji.model.Usluga;
 
+import java.util.List;
+
 public interface UslugaRepository extends JpaRepository<Usluga, Long> {
+    List<Usluga> findByAktywna(Boolean aktywna);
 }
